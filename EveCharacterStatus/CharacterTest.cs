e31746395205f045524b8bc60e2dbc8d7c9fa7ce
baseonmars <dan@baseonmars.co.uk>
2013-04-04 01:27:59 +0100

    More things to do

--- EveCharacterStatus/CharacterTest.cs
+++ EveCharacterStatus/CharacterTest.cs
@@ -10,6 +10,8 @@ namespace EveCharacterStatus
 		public void Should_have_a_name ()
 		{
 			/**
+			 * Part 1.
+			 * 
 			 * Write the character class required to make this test
 			 * pass.
 			 * 
@@ -22,9 +24,40 @@ namespace EveCharacterStatus
 			 * We're not going to populate the character using the API yet
 			 * so don't get side tracked.
 			 */
-			var character = new Character("Hank BangFlex");
-			Assert.AreEqual(character.name, "Hank BangFlex");
+			var character = new Character("Hank Bangflex");
+			Assert.AreEqual(character.name, "Hank Bangflex");
 		}
+		
+		[Test()]
+		public void Should_have_a_first_name ()
+		{
+			/**
+			 * Part 2.
+			 * 
+			 * Add functionality to your character class to make this test pass.
+			 */
+			var Character = new Character("Bank Hangflex");
+			Assert.AreEqual(character.firstName, "Bank");
+		}
+		
+		/**
+		 * Part 3.
+		 * 
+		 * Write a test for the characters Surname and then write
+		 * the functionality into the Character class.
+		 */
+		
+		/**
+		 * Part 4.
+		 * 
+		 * Write a test that checks the surname is correct for a character 
+		 * with the name: "Hank B. Hangflex", then add the functionality
+		 * to your Character class.
+		 * 
+		 * Write another test that checks that the surname is correct 
+		 * for a character called just "Hank", then add the functionality
+		 * to your Character class.
+		 */
 	}
 }
 
