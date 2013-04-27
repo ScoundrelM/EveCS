0275ac2abdb2fb7dd6d2c79e9702b0e6177f7c1a
Matt Boyd <mattuk81@gmail.com>
2013-04-27 18:46:44 +0100

    added api client test

--- EveCharacterStatus/CharacterTest.cs
+++ EveCharacterStatus/CharacterTest.cs
@@ -24,35 +24,35 @@ namespace EveCharacterStatus
 		public void Character_should_have_intelligence ()
 		{
 			var character = new Character("Hank Bangflex");
-			Assert.AreEqual(character.intelligence,0.0);
+			Assert.AreNotEqual(character.intelligence,null);
 		}
 
 		[Test()]
 		public void Character_should_have_perception ()
 		{
 			var character = new Character("Hank Bangflex");
-			Assert.AreEqual(character.perception,0.0);
+			Assert.AreNotEqual(character.perception,null);
 		}	
 
 		[Test()]
 		public void Character_should_have_charisma ()
 		{
 			var character = new Character("Hank Bangflex");
-			Assert.AreEqual(character.charisma,0.0);
+			Assert.AreNotEqual(character.charisma,null);
 		}	
 
 		[Test()]
 		public void Character_should_have_willpower ()
 		{
 			var character = new Character("Hank Bangflex");
-			Assert.AreEqual(character.willpower,0.0);
+			Assert.AreNotEqual(character.willpower,null);
 		}	
 
 		[Test()]
 		public void Character_should_have_memory ()
 		{
 			var character = new Character("Hank Bangflex");
-			Assert.AreEqual(character.memory,0.0);
+			Assert.AreNotEqual(character.memory,null);
 		}	
 
 	}
--- EveCharacterStatus/ClientTest.cs
+++ EveCharacterStatus/ClientTest.cs
@@ -6,11 +6,12 @@ namespace EveCharacterStatus
 	[TestFixture()]
 	public class ClientTest
 	{
-		public ClientTest ()
+		[Test()]
+		public void Should_have_an_api ()
 		{
-
+			ClientForAPI api = new ClientForAPI(1904964, "zesRGv3T3jsETycEd0gChWLvA4VCj1xPN7c5kGgfdwRgrXIE80ARYe1EDcvAUZw7",  268435455);
+			Assert.AreNotEqual(api, null);
 		}
-
 	}
 }
 
