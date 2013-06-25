c302a991cbea1dd42353bf97bedae9c1548f0520
Matt Boyd <mattuk81@gmail.com>
2013-06-25 22:29:11 +0100

    Tests for all the printing in place

--- EveCharacterStatus/AttributeWriterTest.cs
+++ EveCharacterStatus/AttributeWriterTest.cs
@@ -30,7 +30,32 @@ namespace EveCharacterStatus
 
 			Assert.AreEqual ("Base test: , 6",  mockwriter.lastWritten());
 		}
-	
+
+		[Test]
+		public void Should_print_attribute_augmentors()
+		{
+			var mockwriter = new SpyConsoleWriter ();
+			AttributeWriter attributeWriter = new AttributeWriter (mockwriter);
+
+			KeyValuePair<string, double> mockAttribute = new KeyValuePair<string, double> ("test", 3);
+
+			attributeWriter.PrintAttributeAugmentors (mockAttribute);
+
+			Assert.AreEqual ("test Augmentation: , 3", mockwriter.lastWritten ());
+		}
+
+		[Test]
+		public void Should_print_attribute_totals()
+		{
+			var mockwriter = new SpyConsoleWriter ();
+			AttributeWriter attributeWriter = new AttributeWriter (mockwriter);
+
+			KeyValuePair<string, double> mockAttribute = new KeyValuePair<string, double> ("test", 1.5);
+
+			attributeWriter.PrintAttributeTotals (mockAttribute);
+
+			Assert.AreEqual ("test Total: 1.5", mockwriter.lastWritten ());
+		}
 	}
 }
 
