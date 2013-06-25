dc35b2e15abc87c13260d501faeb6401a995a0b3
Matt Boyd <mattuk81@gmail.com>
2013-06-25 23:08:28 +0100

    refactored and reformatted attribute writer rests

--- EveCharacterStatus/AttributeWriterTest.cs
+++ EveCharacterStatus/AttributeWriterTest.cs
@@ -17,49 +17,38 @@ namespace EveCharacterStatus
 			attributeWriter = new AttributeWriter (this.mockWriter);
 		}
 
-
 		[Test()]
 		public void Should_print_attribute_breakdown ()
 		{
 			this.attributeWriter.PrintAttributeBreakdown("Intelligence", 20, 19, 1);
-			
 			Assert.AreEqual("Intelligence: \t\t20 (19) +1", this.mockWriter.lastWritten());
-
 		}
 
 		[Test()]
 		public void Should_print_attribute()
 		{
-		
-			KeyValuePair<string, double> mockAttribute = new KeyValuePair<string, double> ("test", 6);
-
-			this.attributeWriter.PrintAttribute (mockAttribute);
-
+			this.attributeWriter.PrintAttribute (mockAttribute("test",6));
 			Assert.AreEqual ("Base test: , 6",  this.mockWriter.lastWritten());
 		}
 
 		[Test]
 		public void Should_print_attribute_augmentors()
 		{
-
-			KeyValuePair<string, double> mockAttribute = new KeyValuePair<string, double> ("test", 3);
-
-			this.attributeWriter.PrintAttributeAugmentors (mockAttribute);
-
+			this.attributeWriter.PrintAttributeAugmentors (mockAttribute("test",3));
 			Assert.AreEqual ("test Augmentation: , 3", this.mockWriter.lastWritten ());
 		}
 
 		[Test]
 		public void Should_print_attribute_totals()
 		{
-
-			KeyValuePair<string, double> mockAttribute = new KeyValuePair<string, double> ("test", 1.5);
-
-			this.attributeWriter.PrintAttributeTotals (mockAttribute);
-
+			this.attributeWriter.PrintAttributeTotals (mockAttribute("test",1.5));
 			Assert.AreEqual ("test Total: 1.5", this.mockWriter.lastWritten ());
 		}
 
+		private KeyValuePair<string,double> mockAttribute(string name,double value)
+		{
+			return new KeyValuePair<string, double> (name,value);
+		}
 
 
 	}
